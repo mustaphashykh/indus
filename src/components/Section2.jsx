b882618a45e6d5a0874369abe8bab289baa0346a
mustaphashykh <mustafaahmedshaikh125@gmail.com>
2023-08-26 23:36:15 +0500

    Fixed Section2

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -3,7 +3,7 @@ import Home from '../assets/images/Home.svg'
 const Section2 = () => {
     return (
         <div className="text-[#FFF] lg:flex lg:items-center lg:px-28">
-            <div className='w-full flex lg:block flex-col items-center text-center lg:text-left lg:pr-10 pt-24 pb-8'>
+            <div className='w-full flex lg:block flex-col items-center text-center lg:text-left lg:pr-16 pt-24 pb-8'>
                 <h1 className=' text-4xl lg:text-6xl font-bold tracking-wide lg:tracking-tight'>Experience the <span className='text-[#1C75BC]'>Future</span> of <span className='text-[#1C75BC]'>Home</span> Automation</h1>
                 <div className='bg-gray-700 opacity-60 w-28 lg:w-80 h-0.5 rounded my-9'></div>
                 <p className='text-sm lg:text-base px-8 lg:px-0'>
